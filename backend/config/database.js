5ce481ad88b20fc98030b6a0f604c694aec8b616
Ryan McCarthy <ryanmccarthy031@gmail.com>
2020-08-22 22:09:42 -0400

    Changed database config for heroku deployment

--- backend/config/database.js
+++ backend/config/database.js
@@ -4,12 +4,7 @@ module.exports = ({ env }) => ({
     default: {
       connector: 'mongoose',
       settings: {
-        host: env('DATABASE_HOST', 'db'),
-        srv: env.bool('DATABASE_SRV', false),
-        port: env.int('DATABASE_PORT', 27017),
-        database: env('DATABASE_NAME', 'strapi'),
-        username: env('DATABASE_USERNAME', 'strapi'),
-        password: env('DATABASE_PASSWORD', 'password'),
+        host: env('DATABASE_URI'),
       },
       options: {
         authenticationDatabase: env('AUTHENTICATION_DATABASE', null),
